79e24a1c65b6109a947b1b1f7737e290bf154108
npatrick96 <patrickniyongabo@gmail.com>
2017-07-10 21:48:23 -0400

    minor change to test webhook

--- src/main/java/com/gcit/controller/LMSAuthorController.java
+++ src/main/java/com/gcit/controller/LMSAuthorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.gcit.dao.AuthorDao;
 import com.gcit.domain.Author;
-//testing jenkins webhook
+//testing jenkins webhook comment #1
 @RestController
 public class LMSAuthorController {
 
