3ccca412e6ab91404b05f4cd2d43d4670a17126e
npatrick96 <patrickniyongabo@gmail.com>
2017-07-13 15:00:42 -0400

    test comment 12

--- src/main/java/com/gcit/controller/LMSAuthorController.java
+++ src/main/java/com/gcit/controller/LMSAuthorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.gcit.dao.AuthorDao;
 import com.gcit.domain.Author;
-//testing jenkins webhook comment #11
+//testing jenkins webhook comment #12
 @RestController
 public class LMSAuthorController {
 
